a3e95ba0ad4e6dc43dbae15778496f0a25ecffc1
Josh Correia <josh.correia@kove.com>
2023-11-27 00:02:11 -0800

    Use 2 spaces instead of 4

--- script.js
+++ script.js
@@ -10,18 +10,18 @@
 // @namespace    https://greasyfork.org/users/1220845
 // ==/UserScript==
 
-function add_links() {
-    const cert_numbers = document.querySelectorAll(".collectible-listing td.collectible-listing-cert");
+function add_links(number) {
+  const cert_numbers = document.querySelectorAll(".collectible-listing td.collectible-listing-cert");
 
-    cert_numbers.forEach(function(cert) {
-        var cert_number = cert.innerText;
-        var a_link = document.createElement('a');
-        a_link.setAttribute('href', `https://www.cgccards.com/certlookup/${cert_number}`);
-        a_link.innerHTML = cert_number;
-        a_link.target="_blank"; // open in a new tab
-        cert.innerHTML = "";
-        cert.appendChild(a_link);
-    })
+  cert_numbers.forEach(function(cert) {
+    var cert_number = cert.innerText;
+    var a_link = document.createElement('a');
+    a_link.setAttribute('href', `https://www.cgccards.com/certlookup/${cert_number}`);
+    a_link.innerHTML = cert_number;
+    a_link.target="_blank"; // open in a new tab
+    cert.innerHTML = "";
+    cert.appendChild(a_link);
+  })
 }
 
 // TODO: Add MutationObserver instead of polling
